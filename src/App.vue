3de1ab644f08ca8085847402f2d7d564a91b027f
xin.shen <969187979@qq.com>
2020-01-09 16:50:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    hello world
+    <div>
+      {{msg}}
+    </div>
+    <div class="title">
+      {{ name }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'shenxin',
+      name: ''
+    }
+  },
+  methods: {
+    print() {
+      this.name = "vue success"
+    }
+  },
+  mounted() {
+    this.print()
+  }
+}
+</script>
+<style lang="scss">
+.wrapper {
+  font-size: 40px;
+  .title {
+    color: red;
+  }
+}
+</style>
